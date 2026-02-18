2cdaeccdb44eafbc27c691b6ac399039501a727d
Subham Singh <subham@weetechsolution.com>
2026-02-18 09:57:59 +0530

    Configure production API URL and improve workflow step naming

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api" || "https://crud-api-1.onrender.com/api",
+  baseURL: API_BASE_URL,
 });
